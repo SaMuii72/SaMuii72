72a92bf778dea34a3c628d00995323a102441919
SaMuii72 <113111313+SaMuii72@users.noreply.github.com>
2022-11-02 18:21:14 +0700

    Add files via upload

--- Doo streamer/model/streamer.js
+++ Doo streamer/model/streamer.js
@@ -1,1 +1,41 @@
+const mongoose = require("mongoose")
+const streamerSchema =  mongoose.Schema({
+    Name: {
+        type: String,
+    },
+    Description:{
+        type: String,
+    },
+    Profile:{
+        type: String,
+    },
+    Img:{
+        type: String,
+    },
+    Follower:{
+        type: String,
+    },
+    Game:{
+        type: String,
+    },
+    Platform:{
+        type: String,
+    },
+    Stream:{
+        type: String,
+    },
+    Facebook:{
+        type: String,
+    },
+    Tiktok:{
+        type: String,
+    },
+    Youtube:{
+        type: String,
+    },
+    Instargram:{
+        type: String,
+    },
+});
 
+exports.Streamer = mongoose.model("streamer",streamerSchema)
