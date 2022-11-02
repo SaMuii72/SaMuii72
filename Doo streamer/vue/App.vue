7569bbc5c3239c2fe4652701cfa0f4ee8d747443
SaMuii72 <113111313+SaMuii72@users.noreply.github.com>
2022-11-02 18:32:47 +0700

    Add files via upload

---
+++ Doo streamer/vue/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="app">
+  <nav>
+    <ul>
+    <li><router-link to="/" class="active">Home</router-link></li>
+    </ul>
+  </nav>
+  <router-view/>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap');
+* {
+  font-family:  'Rubik', sans-serif;
+  margin: 0;
+}
+
+ul {
+  text-align: left;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+li{
+  display: inline-block;
+}
+
+a{
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 10px 10px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+nav{
+  background: black;
+  padding: 14px 0px;
+  overflow: hidden;
+}
+</style>
+
+
+
---
+++ Doo streamer/vue/web.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="web">
+    <nav>
+      <router-link to="/">index</router-link> 
+    </nav>
+    <router-view/>
+    </div>
+</template>
